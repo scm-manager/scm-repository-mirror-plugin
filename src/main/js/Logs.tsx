9073ae24cac4e3e85a36b47ec2511357c1263048
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2021-06-03 14:35:27 +0200

    Use monospace font for logs

--- src/main/js/Logs.tsx
+++ src/main/js/Logs.tsx
@@ -108,7 +108,7 @@ type LogLinesProps = {
 };
 
 const LogLines: FC<LogLinesProps> = ({ lines }) => (
-  <ul className="m-3 p-3 has-background-white-ter">
+  <ul className="m-3 p-3 has-background-white-ter is-family-monospace is-size-7 has-text-black">
     {lines.map(line => (
       <li key={line}>{line}</li>
     ))}
